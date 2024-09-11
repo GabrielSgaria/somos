4bc526cbf8cff1510364fceb1312f98b7837382c
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-09-11 13:08:52 -0300

    fix cards

--- src/app/components/card-services.tsx
+++ src/app/components/card-services.tsx
@@ -1,22 +1,42 @@
+'use client'
 import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
 
 interface CardServicesProps {
-    className?: string,
-    classNameText?: string,
-    title: string,
-    text: string,
+    title: string;
+    text: string;
+    highlighted?: boolean;
+    isLeft?: boolean;
 }
 
-export function CardServices({ className, title, text, classNameText }: CardServicesProps) {
+export function CardServices({ title, text, highlighted = false, isLeft = true }: CardServicesProps) {
+
+    const containerClass = highlighted
+        ? "bg-blue-500 text-gray-50"
+        : "bg-gray-100 text-gray-950";
+
+    const textClass = highlighted
+        ? "text-gray-50"
+        : "text-gray-600";
+
+
+    const initialX = isLeft ? -200 : 200;
+
     return (
-        <div className={cn(`w-full h-full max-h-96 rounded-xl shadow-xl`, className)}>
+        <motion.div
+            initial={{ opacity: 0, x: initialX }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: isLeft ? 0.1 : 0.2 }}
+            className={cn(`w-full h-full max-h-96 rounded-xl shadow-xl`, containerClass)}
+        >
             <div className="p-5 flex flex-col gap-3">
                 <h1 className={cn(`text-2xl font-extrabold`)}>
-                    {title} 
+                    {title}
                 </h1>
-                <span className={cn(`text-lg text-wrap`,  classNameText)}>{text}</span>
-
+                <span className={cn(`text-lg text-wrap`, textClass)}>
+                    {text}
+                </span>
             </div>
-        </div>
-    )
+        </motion.div>
+    );
 }
--- src/app/components/section-services.tsx
+++ src/app/components/section-services.tsx
@@ -1,5 +1,70 @@
-import { services } from "@/lib/services";
+
+
 import { CardServices } from "./card-services";
+import Image from "next/image";
+
+const servicesCard = [
+    {
+        title: "Organização Impecável",
+        text: "Organizamos cada detalhe do imóvel para receber seus hóspedes com conforto e estilo, garantindo uma experiência de primeira classe.",
+        highlighted: true,
+        isLeft: true
+    },
+    {
+        title: "Mapeamento de Preços",
+        text: "Realizamos um mapeamento estratégico para definir o melhor preço, maximizando suas reservas e mantendo a competitividade no mercado.",
+        highlighted: false,
+        isLeft: true
+    },
+    {
+        title: "Sessão de Profissionais",
+        text: "Oferecemos uma sessão fotográfica profissional para destacar o imóvel e atrair mais hóspedes, capturando a essência de cada espaço.",
+        highlighted: false,
+        isLeft: true
+    },
+    {
+        title: "Criação de Anúncios Poderosos",
+        text: "Criamos anúncios impactantes que comunicam claramente o conceito do imóvel e suas vantagens, atraindo o público ideal.",
+        highlighted: true,
+        isLeft: true
+    },
+    {
+        title: "Coordenação de Check-in e Check-out",
+        text: "Coordenamos todo o processo de check-in e check-out, além de garantir uma limpeza impecável para o próximo hóspede.",
+        highlighted: true,
+        isLeft: true
+    },
+    {
+        title: "Atendimento Personalizado",
+        text: "Oferecemos um atendimento dedicado e eficiente para resolver qualquer necessidade dos hóspedes durante sua estadia.",
+        highlighted: false,
+        isLeft: false
+    },
+    {
+        title: "Suporte ao Hóspede",
+        text: "Disponibilizamos suporte ao hóspede 24/7, garantindo uma experiência tranquila e sem imprevistos durante a estadia.",
+        highlighted: false,
+        isLeft: false
+    },
+    {
+        title: "Reservas Diretas no Site",
+        text: "Facilitamos o processo de reservas diretas por meio de um site dedicado, garantindo praticidade e segurança para o hóspede.",
+        highlighted: true,
+        isLeft: false
+    },
+    {
+        title: "Gestão Multiplataforma",
+        text: "Realizamos a gestão do imóvel em várias plataformas de locação, aumentando a visibilidade e potencial de reservas.",
+        highlighted: true,
+        isLeft: false
+    },
+    {
+        title: "Manutenção e Inventário",
+        text: "Cuidamos de eventuais manutenções e mantemos o inventário atualizado, garantindo que o imóvel esteja sempre em perfeitas condições.",
+        highlighted: false,
+        isLeft: false
+    },
+];
 
 export function SectionServices() {
     return (
@@ -11,16 +76,27 @@ export function SectionServices() {
                     <span className="text-center font-semibold text-xl text-gray-500 max-w-[700px]">Oferecemos um serviço completo de hospedagem, do check-in ao atendimento personalizado.</span>
                 </div>
 
-                <div className="grid grid-cols-1 md:columns-2 md:block md:gap-x-5 md:space-y-5 gap-y-4 gap-x-3 md:px-20 ">
-                    {services.map((service, i) => (
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-y-4 gap-x-3 md:px-44">
+                    {servicesCard.map((service, i) => (
                         <CardServices
                             key={i}
                             title={service.title}
                             text={service.text}
-                            className={i % 2 === 0 ? "bg-blue-500 text-gray-50" : "bg-gray-100 text-gray-950"}
-                            classNameText={i % 2 === 0 ? "text-gray-50" : "text-gray-600"} />
+                            highlighted={service.highlighted}
+                            isLeft={i % 2 === 0} 
+                        />
                     ))}
-
+                </div>
+                <div className="mt-32 w-full max-w-[1200px] h-[508px] relative mx-auto rounded-xl shadow-xl overflow-hidden">
+                    <Image
+                        alt="Image Somos"
+                        src="/image/somos-banner.png"
+                        fill
+                        quality={100}
+                        priority
+                        sizes="(min-width: 1200px) 50vw, 100vw"
+                        className="object-right md:object-center object-cover"
+                    />
                 </div>
             </div>
 
